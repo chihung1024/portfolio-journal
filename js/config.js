675245cddd47cce4f0af26254a42e3f034c46da3
chihung1024 <104315542+chihung1024@users.noreply.github.com>
2025-08-06 10:11:25 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// =========================================================================================
+// == 設定模組 (config.js)
+// =========================================================================================
+
+// 您的 Firebase 專案設定
+// 請務必替換成您自己的真實金鑰
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_yBMNUBUK6yGClPJ4BwbMrFFh4pDHqYc",
+  authDomain: "portfolio-journal-467915.firebaseapp.com",
+  projectId: "portfolio-journal-467915",
+  storageBucket: "portfolio-journal-467915.firebasestorage.app",
+  messagingSenderId: "951186116587",
+  appId: "1:951186116587:web:9a630cf9b4b1c0893613ae",
+  measurementId: "G-6C3M8W9RH7"
+};
+
+// 您的後端 API 設定
+export const API = {
+  URL: 'https://portfolio-journal-api-951186116587.asia-east1.run.app', // <-- 請確認這是您自己的 Cloud Run 服務網址
+  KEY: 'QqcUhBV04KciA1xeDibpOmcLpAQWDt' // <-- 請確認這是您自己的金鑰
+};
